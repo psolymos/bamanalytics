34c9b9191eed151ee582dbb5c2e5acd3d5492ae4
Peter Solymos <psolymos@gmail.com>
2015-05-07 14:35:59 -0600

    more or less finished (waiting on SS)

--- R/dataprocessing.R
+++ R/dataprocessing.R
@@ -1,18 +1,42 @@
+##---
+##title: "Data processing for nationam BAM analyses"
+##author: "Peter Solymos"
+##date: "May 7, 2015"
+##output: 
+##  pdf_document: 
+##    toc: true
+##    toc_depth: 2
+##---
+
+### Preliminaries
+
+## Define root folder where data are stored
 ROOT <- "c:/bam/May2015"
 
+## Load required packages
 library(mefa4)
 library(RODBC)
+library(maptools)
+library(pbapply)
+library(detect)
 
+## Load functions kept in separate file
+source("~/repos/bamanalytics/R/dataprocessing_functions.R")
+
+### Pulling in tables
+
+## Define MS Access database connection
 con <- odbcConnectAccess2007(file.path(ROOT, "BAM_BayneAccess_BAMBBScore.accdb"))
 
 #### SS level stuff for BAM+BBS combined
-## time zone, BCR, jurisdiction, XY
+
+## This table has time zone, BCR, jurisdiction, XY
 SS01 <- sqlFetch(con, "dbo_BBSBAM_V4_XYTBL_ATTRIBUTES1")
 SS01 <- nonDuplicated(SS01, SS, TRUE)
 SS01$COUNTRY <- ifelse(SS01$JURSALPHA %in% c("AB","BC","MB","NB",
     "NL","NS","NT","NU","ON","PE","QC","SK","YT"), "CAN", "USA")
 
-## tree
+## Tree proportions
 SS02 <- sqlFetch(con, "dbo_TREE_BBSBAM_V4_tbl")
 SS02 <- nonDuplicated(SS02, SS, TRUE)
 SS02 <- SS02[rownames(SS01),]
@@ -23,13 +47,12 @@ SS02$TREE3[SS02$TREE < 0.25] <- "Open"
 SS02$TREE3[SS02$TREE >= 0.25 & SS02$TREE < 0.60] <- "Sparse"
 SS02$TREE3[SS02$TREE >= 0.60] <- "Dense"
 
-
-## point level land cover
+## Point level land cover
 SS03 <- sqlFetch(con, "dbo_BAMBBS_LANDCOVER_PTS")
 SS03 <- nonDuplicated(SS03, SS, TRUE)
 SS03 <- SS03[rownames(SS01),]
 
-## LCC05 classes
+## Reclass LCC05
 ltlcc <- read.csv("~/repos/bamanalytics/lookup/lcc05.csv")
 SS03$LCC05_PT[SS03$LCC05_PT < 1 | SS03$LCC05_PT > 39] <- NA
 SS03$LCC05_PT[SS01$COUNTRY == "USA"] <- NA
@@ -38,7 +61,7 @@ SS03$HAB_LCC2 <- ltlcc$BAMLCC05V2_label2[match(SS03$LCC05_PT, ltlcc$lcc05v1_2)]
 SS03$HAB_LCC1 <- relevel(SS03$HAB_LCC1, "ConifDense")
 SS03$HAB_LCC2 <- relevel(SS03$HAB_LCC2, "Conif")
 
-## reclass EOSD
+## Reclass EOSD
 lteosd <- read.csv("~/repos/bamanalytics/lookup/eosd.csv")
 levels(SS03$EOSD_PT) <- sub(",", "", levels(SS03$EOSD_PT))
 SS03$EOSD_PT <- as.integer(as.character(SS03$EOSD_PT))
@@ -48,7 +71,7 @@ SS03$HAB_EOSD2 <- lteosd$Reclass_label2[match(SS03$EOSD_PT, lteosd$Value)]
 SS03$HAB_EOSD1 <- relevel(SS03$HAB_EOSD1, "ConifDense")
 SS03$HAB_EOSD2 <- relevel(SS03$HAB_EOSD2, "Conif")
 
-## reclass NALCMS
+## Reclass NALCMS
 ltnalc <- read.csv("~/repos/bamanalytics/lookup/nalcms.csv")
 SS03$HAB_NALC2 <- ltnalc$Label[match(SS03$NALCMS_PT, ltnalc$Value)]
 tmp <- as.character(interaction(SS03$HAB_NALC2, SS02$TREE3, sep="", drop=TRUE))
@@ -70,7 +93,7 @@ SS03$LCC_OFF2[SS03$HAB_NALC1 %in% c("ConifDense","MixedDense")] <- "1"
 SS03$LCC_combo <- SS03$LCC_OFF1
 SS03$LCC_combo[is.na(SS03$LCC_OFF1)] <- SS03$LCC_OFF2[is.na(SS03$LCC_OFF1)]
 
-## main SS level object
+## Put together the main SS level object
 SS <- data.frame(
     PCODE=SS01$PCODE,
     SS=SS01$SS,
@@ -86,7 +109,9 @@ SS <- data.frame(
     SS03[,c("HAB_LCC1", "HAB_LCC2", "HAB_EOSD1", "HAB_EOSD2", 
         "HAB_NALC2", "HAB_NALC1", "LCC_combo")])
 
-#### project summary table -- locally tweaked
+#### Project summary table
+
+## This table needed local tweaks to be operable
 #PCODE <- sqlFetch(con, "dbo_National_Proj_Summary_V4_2015")
 #PCODE$SSMA_TimeStamp <- NULL
 PCODE <- read.csv(file.path(ROOT,"proj.csv"))
@@ -97,13 +122,13 @@ PCODE$Maxdist <- droplevels(PCODE$Maxdist)
 PCODE$Maxdist <- as.numeric(as.character(PCODE$Maxdist))
 PCODE$Maxdur <- pmin(PCODE$MaxDuration, 10)
 
+#### Survey level fields
 
-#### survey level stuff
-
+## Pull in point count tables
 pkbam <- sqlFetch(con, "dbo_National_PKEY_V4_2015")
 pkbam$SSMA_TimeStamp <- NULL
 pkbbs <- sqlFetch(con, "dbo_PKEY_BBS_V3_2015")
-
+## What columns to retain
 PKCOLS <- c("PKEY","SS","PCODE","METHOD","SITE","STN","ROUND",
     "YEAR","MONTH","DAY","HOUR","MIN","PART","MAXDUR","MAXDIS")
 
@@ -131,20 +156,30 @@ PKEY <- rbind(pkbam, pkbbs)
 rm(pkbam, pkbbs)
 gc()
 
-#### offsets
+## Map `METHOD` field from project summary table onto `PKEY$METHOD`
+## so that duration and distance method can be carried forward to 
+## point count table
+levels(PCODE$Method)[levels(PCODE$Method) == "QCAtlas:118"] <- "QCATLAS:118"
+compare.sets(PCODE$Method, PKEY$METHOD)
+setdiff(PKEY$METHOD, PCODE$Method)
+setdiff(PCODE$Method, PKEY$METHOD)
+PKEY$DURMETH <- PCODE$DURMETH[match(PKEY$METHOD, PCODE$Method)]
+PKEY$DISMETH <- PCODE$DISTMETH[match(PKEY$METHOD, PCODE$Method)]
 
+#### Offset specific variables
+
+## Date/time components
 MM <- ifelse(PKEY$MONTH < 10, paste0("0", PKEY$MONTH), as.character(PKEY$MONTH))
 HH <- ifelse(PKEY$HOUR < 10, paste0("0", PKEY$HOUR), as.character(PKEY$HOUR))
 mm <- ifelse(PKEY$MIN < 10, paste0("0", PKEY$MIN), as.character(PKEY$MIN))
 DD <- with(PKEY, paste0(YEAR, "-", MM, "-", DAY, " ", HH, ":", mm, ":00"))
 DD <- strptime(DD, "%Y-%m-%e %H:%M:%S")
+## Julian day
 PKEY$JDAY <- DD$yday / 365
 summary(PKEY$JDAY)
 ## prevent too far extrapolation
 PKEY$JDAY[PKEY$JDAY < 0.35 | PKEY$JDAY > 0.55] <- NA
-
-## TSSR
-library(maptools)
+## TSSR = time since sunrise
 Coor <- as.matrix(cbind(as.numeric(SS$X),as.numeric(SS$Y)))[match(PKEY$SS, rownames(SS)),]
 JL <- as.POSIXct(DD)
 subset <- rowSums(is.na(Coor))==0 & !is.na(JL)
@@ -162,99 +197,17 @@ PKEY$TSSR[PKEY$start_time > 12] <- NA ## after noon
 summary(PKEY$TSSR)
 summary(PKEY$start_time)
 
+#### Calculate the offsets (optional)
+if (FALSE) { # BEGIN offset calculations
+
 offdat <- data.frame(PKEY[,c("PKEY","SS","TSSR","JDAY","MAXDUR","MAXDIS")],
     SS[match(PKEY$SS, rownames(SS)),c("LCC_combo","TREE")])
 offdat$srise <- PKEY$srise + PKEY$MDT_offset
 summary(offdat)
 
-library(pbapply)
-library(detect)
 load_BAM_QPAD(version=1)
 BAMspp <- getBAMspecieslist()
 load("~/Dropbox/abmi/intactness/dataproc/BAMCOEFS25.Rdata")
-
-#i="OVEN";j=1
-#x=DAT1[,c("QPADLCCcombo","TREE","JDAY","TSSR","MAXDIS","MAXDUR")]
-offset_fun <- function(j, i, x) {
-    if (i %in% BAMspp) { ## spp with offsets (>=75 obs)
-        type <- "full"
-    } else {
-        if (i %in% BAMCOEFS25$spp) { ## SPP based on >=25 obs
-            type <- "const"
-        } else {
-            stop("no offsets available")
-        }
-    }
-    if (type == "full") {
-        xna <- as.data.frame(lapply(x[,c("LCC_combo","TREE","JDAY","TSSR")], 
-            function(z) as.integer(is.na(z))))
-        xna <- with(xna, interaction(LCC_combo,TREE,JDAY,TSSR, drop=TRUE, sep=""))
-        xx <- lapply(levels(xna), function(z) x[xna==z,])
-        id <- lapply(levels(xna), function(z) which(xna==z))
-        names(xx) <- levels(xna)
-        off <- lapply(xx, function(z) offset_fun0(j, i, x=z, type=type))
-        OFF <- numeric(nrow(x))
-        for (k in seq_len(length(off)))
-            OFF[id[[k]]] <- off[[k]]
-        gc <- globalBAMcorrections(i, 
-            r=x$MAXDIS[!is.finite(OFF)],
-            t=x$MAXDUR[!is.finite(OFF)])
-        gc <- rowSums(log(as.matrix(gc)))
-        OFF[!is.finite(OFF)] <- gc
-    } else {
-        OFF <- offset_fun0(j, i, x=x, type=type)
-    }
-    OFF
-}
-
-offset_fun0 <- function(j, i, x, type=c("full","const")) {
-    BOOT <- j != 1
-    if (type == "full") { ## spp with offsets (>=75 obs)
-        ml1 <- getBAMmodellist()$sra
-        ml2 <- getBAMmodellist()$edr
-        if (any(is.na(x$JDAY)))
-            ml1 <- ml1[!grepl("JDAY", ml1)]
-        if (any(is.na(x$TSSR)))
-            ml1 <- ml1[!grepl("TSSR", ml1)]
-        if (any(is.na(x$TREE)))
-            ml2 <- ml2[!grepl("TREE", ml2)]
-        if (any(is.na(x$LCC_combo)))
-            ml2 <- ml2[!grepl("LCC", ml2)]
-        best <- bestmodelBAMspecies(i, 
-            model.sra=names(ml1), model.edr=names(ml2),
-            type=ifelse(BOOT, "multi", "BIC"))
-        out <- with(x, localBAMcorrections(i,
-            r=MAXDIS,
-            t=MAXDUR,
-            jday=JDAY, 
-            tssr=TSSR, 
-            tree=TREE, 
-            lcc=LCC_combo,
-            model.sra=best$sra, 
-            model.edr=best$edr,
-            boot=BOOT, ## MVN approach of j != 1
-            ver=1))
-    }
-    if (type == "const") { ## SPP based on >=25 obs
-        if (BOOT) {
-            PHI <- exp(rnorm(1, 
-                mean = BAMCOEFS25$sra_estimates[[i]][["0"]]$coef[1],
-                sd = BAMCOEFS25$sra_estimates[[i]][["0"]]$vcov[1,1]))
-            TAU <- exp(rnorm(1, 
-                mean = BAMCOEFS25$edr_estimates[[i]][["0"]]$coef[1],
-                sd = BAMCOEFS25$edr_estimates[[i]][["0"]]$vcov[1,1]))
-        } else {
-            PHI <- exp(BAMCOEFS25$sra_estimates[[i]][["0"]]$coef[1])
-            TAU <- exp(BAMCOEFS25$edr_estimates[[i]][["0"]]$coef[1])
-        }
-        out <- customBAMcorrections(r=x$MAXDIS,
-            t=x$MAXDUR,
-            phi=PHI,
-            tau=TAU)
-    }
-    #out
-    rowSums(log(as.matrix(out)))
-}
 
 (sppp <- union(BAMspp, BAMCOEFS25$spp))
 
@@ -285,62 +238,173 @@ save(offdat, file=file.path(ROOT, "out",
 write.csv(offdat, row.names=FALSE, file=file.path(ROOT, "out",
     paste0("offset_covariates_", Sys.Date(), ".csv")))
 
+} # END offset calculations
 
 
+#### Point count tables and methodology
 
-
-
-
-#### methodology stuff
+## Some of these tables are also tweaked locally (see below)
 BEH <- sqlFetch(con, "dbo_DD_DescripBEH")
-DISINT <- sqlFetch(con, "dbo_DD_DescripDistance")
-DISINT$SSMA_TimeStamp <- NULL
-DURINT <- sqlFetch(con, "dbo_DD_DescripPeriod")
+#DISINT <- sqlFetch(con, "dbo_DD_DescripDistance")
+#DISINT$SSMA_TimeStamp <- NULL
+#DURINT <- sqlFetch(con, "dbo_DD_DescripPeriod")
 DISMET <- sqlFetch(con, "dbo_DD_distance_codes_methodology")
 DURMET <- sqlFetch(con, "dbo_DD_duration_codes_methodology")
+## Trailing whitespace removed from factor levels
+levels(DISMET$DISTANCECODE) <- gsub(" *$", "", levels(DISMET$DISTANCECODE))
+levels(DURMET$DURATIONCODE) <- gsub(" *$", "", levels(DURMET$DURATIONCODE))
+#### Point count tables
 
-
-#### counts
-
-pc1 <- sqlFetch(con, "dbo_National_PtCount_V4_2015")
-pc1$SSMA_TimeStamp <- NULL
-pc2 <- sqlFetch(con, "dbo_PointCount_BBS_V3_2015")
-
-
+pcbam <- sqlFetch(con, "dbo_National_PtCount_V4_2015")
+pcbam$SSMA_TimeStamp <- NULL
+pcbbs <- sqlFetch(con, "dbo_PointCount_BBS_V3_2015")
+colnames(pcbbs) <- toupper(colnames(pcbbs))
+colnames(pcbbs)[colnames(pcbbs) == "SPECIES_ID"] <- "SPECIES"
+colnames(pcbbs)[colnames(pcbbs) == "PERIOD"] <- "DURATION"
+pcbbs$PCODE <- "BBS"
+## Columns to keep
+pccols <- c("PCODE","SS","PKEY","DURATION","DISTANCE",
+    "SPECIES","ABUND","BEH")
+## Close the database connection
 close(con)
 
-PCODE <- proj
-SS <- xy
-PKEY <- pk
-PCTBL <- pc
-
-
-#levels(PCODE$DISTMETH)
-#levels(PCODE$DISTMETH) <- toupper(levels(PCODE$DISTMETH))
-DISMET
-levels(DISMET$DISTANCECODE)
-levels(DISMET$DISTANCECODE) <- gsub(" *$", "", levels(DISMET$DISTANCECODE))
-
-levels(PCODE$DURMETH)
-DURMET
-levels(DURMET$DURATIONCODE)
-levels(DURMET$DURATIONCODE) <- gsub(" *$", "", levels(DURMET$DURATIONCODE))
-
-
-## excluding non-aerial detections
-BEH
-sort(100 * table(PCTBL$BEH) / sum(table(PCTBL$BEH)))
+## Duration and distance intervals (locally tweaked)
+DURINT <- read.csv("~/repos/bamanalytics/lookup/durint.csv")
+DISINT <- read.csv("~/repos/bamanalytics/lookup/disint.csv")
+DURINT$dur <- paste0(DURINT$Dur_Start, "-", DURINT$DUR_end)
+DISINT$dis <- paste0(DISINT$DIST_START, "-", DISINT$DIST_END)
+rownames(DURINT) <- DURINT[,1]
+rownames(DISINT) <- DISINT[,1]
+## Combined point count table
+PCTBL <- rbind(pcbam[,pccols], pcbbs[,pccols])
+## Mapping duration and distance intervals 
+PCTBL$dur <- as.factor(DURINT$dur[match(PCTBL$DURATION, rownames(DURINT))])
+PCTBL$dis <- as.factor(DISINT$dis[match(PCTBL$DISTANCE, rownames(DISINT))])
+## Identifying roadside surveys
+PCTBL$ROAD <- 0L
+treat.as.bbs <- c("BBS", "HOBBS","CF","MNBBA")
+PCTBL$ROAD[PCTBL$PCODE %in% treat.as.bbs] <- 1L
+## Filtering behaviour
+#sort(100 * table(PCTBL$BEH) / sum(table(PCTBL$BEH)))
 ## 1=Heard
 ## 11=no birds observed at station - added 2011
 ## 6=seen and heard
-PCTBL <- PCTBL[PCTBL$BEH %in% c(1,11,6),]
-
-## excluding >10 min intervals
-DURINT
+## Excluding non-aerial detections
+keep <- rep(TRUE, nrow(PCTBL))
+keep[PCTBL$BEH %in% c("1","6","Flock")] <- FALSE
+## Excluding >10 min intervals
 ## 10=10-20
 ## 11=0-20
 ## 3=before or after
 ## 8=unk
 ## 9=10-15
-PCTBL <- PCTBL[!(PCTBL$DURATION %in% c(10,11,3,8,9)),]
+keep[PCTBL$DURATION %in% c(10,11,3,8,9)] <- FALSE
+## Excluding unknown distance bands
+keep[PCTBL$DISTANCE %in% c(4,5,9)] <- FALSE
+## Excluding NA values
+keep[is.na(PCTBL$dur)] <- FALSE
+keep[is.na(PCTBL$dis)] <- FALSE
+keep[is.na(PCTBL$ABUND)] <- FALSE
+## Actual filtering
+PCTBL <- PCTBL[keep,]
+## Excluding/dropping species
+PCTBL$SPECIES <- droplevels(PCTBL$SPECIES)
+levels(PCTBL$SPECIES) <- toupper(levels(PCTBL$SPECIES))
+sp <- levels(PCTBL$SPECIES)
+spp.to.exclude <- rep(FALSE, length(sp))
+spp.to.exclude[sp == "NONE"] <- TRUE
+spp.to.exclude[sp == "DUCK"] <- TRUE
+spp.to.exclude[nchar(sp) > 4] <- TRUE
+spp.to.exclude[substr(sp, 1, 2) == "UN"] <- TRUE
+levels(PCTBL$SPECIES)[spp.to.exclude] <- "NONE"
+## Excluding columns
+PCTBL$DURATION <- NULL
+PCTBL$DISTANCE <- NULL
+PCTBL$BEH <- NULL
+PCTBL$dur <- droplevels(PCTBL$dur)
+PCTBL$dis <- droplevels(PCTBL$dis)
+
+compare.sets(SS$SS, PKEY$SS)
+compare.sets(SS$SS, PCTBL$SS)
+compare.sets(PKEY$PKEY, PCTBL$PKEY)
+
+## Save a data backage for new offsets
+dat <- data.frame(PKEY[,c("PKEY","SS","TSSR","JDAY","MAXDUR","MAXDIS","METHOD",
+    "DURMETH","DISMETH")],
+    SS[match(PKEY$SS, rownames(SS)),c("TREE","TREE3","LCC_combo","HAB_NALC1","HAB_NALC2")])
+dat <- droplevels(dat)
+rownames(dat) <- dat$PKEY
+ii <- intersect(dat$PKEY, levels(PCTBL$PKEY))
+dat <- droplevels(dat[ii,])
+summary(dat)
+colSums(is.na(dat))
+## besides `dat` we also need specific aoutput from `PCTBL`
+## and also the methodology x interval lookup
+
+compare.sets(DISMET$DISTANCECODE, PKEY$DISMETH)
+compare.sets(DURMET$DURATIONCODE, PKEY$DURMETH)
+
+PCTBL$DISMETH <- droplevels(PKEY$DISMETH[match(PCTBL$PKEY, PKEY$PKEY)])
+PCTBL$DURMETH <- droplevels(PKEY$DURMETH[match(PCTBL$PKEY, PKEY$PKEY)])
+
+
+## Oddities that should not happen:
+PCTBL$dur[with(PCTBL, DURMETH=="A" & dur=="0-3")] <- "0-10"
+PCTBL$dur[with(PCTBL, DURMETH=="B" & dur=="5-8")] <- "0-5"
+
+PCTBL$dis[with(PCTBL, DISMETH=="B" & dis=="0-Inf")] <- "0-50" # best guess
+PCTBL$dis[with(PCTBL, DISMETH=="C" & dis=="0-Inf")] <- "0-50" # best guess
+PCTBL$dis[with(PCTBL, DISMETH=="F")] <- "0-100" # all kinds of weird stuff
+PCTBL$dis[with(PCTBL, DISMETH=="I" & dis=="100-125")] <- "0-25"
+PCTBL$dis[with(PCTBL, DISMETH=="I" & dis=="100-Inf")] <- "0-25" # best guess
+#PCTBL$dis[with(PCTBL, DISMETH=="L" & dis=="150-Inf")] <- "100-150" # no >150
+#PCTBL$dis[with(PCTBL, DISMETH=="T" & dis=="100-Inf")] <- "-" # no >100
+PCTBL$dis[with(PCTBL, DISMETH=="U" & dis=="0-50")] <- "40-50"
+PCTBL$dis[with(PCTBL, DISMETH=="U" & dis=="100-150")] <- "125-150"
+PCTBL$dis[with(PCTBL, DISMETH=="U" & dis=="100-Inf")] <- "150-Inf"
+PCTBL$dis[with(PCTBL, DISMETH=="U" & dis=="50-Inf")] <- "150-Inf"
+PCTBL$dis[with(PCTBL, DISMETH=="W" & dis=="150-Inf")] <- "100-Inf"
+PCTBL$dis[with(PCTBL, DISMETH=="W" & dis=="100-125")] <- "100-Inf"
+
+PCTBL$dis <- droplevels(PCTBL$dis)
+PCTBL$dur <- droplevels(PCTBL$dur)
+
+durmat <- as.matrix(Xtab(~ DURMETH + dur, PCTBL))
+durmat[durmat > 0] <- 1
+dismat <- as.matrix(Xtab(~ DISMETH + dis, PCTBL))
+dismat[dismat > 0] <- 1
+
+arrange.intervals <-
+function(x, sep="-")
+{
+    x <- ifelse(x > 0, 1L, 0L)
+    ## start/stop
+    ss <- strsplit(colnames(x), sep)
+    names(ss) <- colnames(x)
+    ss <- lapply(ss, as.numeric)
+    ss <- do.call(rbind, ss)
+    nr <- nrow(x)
+    nc <- ncol(x)
+    End <- Id <- array(NA, dim(x))
+    rownames(End) <- rownames(Id) <- rownames(x)
+    for (i in seq_len(nr)) {
+        id <- which(x[i,] > 0)
+        #cn <- colnames(x)[id]
+        endv <- ss[id,2]
+        id <- id[order(endv)]
+        endv <- endv[order(endv)]
+        End[i, seq_len(length(endv))] <- endv
+        Id[i, seq_len(length(endv))] <- id
+    }
+    lc <- which(rev(cumsum(rev(nr - colSums(is.na(Id))))) < 1)[1L] - 1L
+    list(x=x, end=End[,seq_len(lc)], id=Id[,seq_len(lc)])
+}
+ltdur <- arrange.intervals(durmat)
+ltdis <- arrange.intervals(dismat)
+
+save(dat, PCTBL, ltdur, ltdis, 
+    file=file.path(ROOT, "out",
+    paste0("new_offset_data_package_", Sys.Date(), ".Rdata")))
+
+
 
