b038402dbadd6b0acd0c4ade3f369b5416f54b3f
Peter Solymos <psolymos@gmail.com>
2016-03-03 09:45:52 -0700

    project based estimates

--- R/qpad-duration-ms.R
+++ R/qpad-duration-ms.R
@@ -172,6 +172,104 @@ save(resDur, resDurData,
 
 ## project-wise estimation -------------------------------------------
 
+## models to consider
+NAMES <- list("0"="INTERCEPT")
+ff <- list(~ 1)
+
+## define pcode, and excl (excl=T to exclude pcode, excl=F to use only pcode)
+fitDurFun2 <- function(spp, fit=TRUE, type=c("rem","mix"), pcode=NULL, excl=TRUE) {
+    rn <- intersect(rownames(pkDur), rownames(xtDur[[spp]]))
+    X0 <- pkDur[rn,]
+    if (!is.null(pcode)) {
+        X0 <- if (excl)
+            X0[X0$PCODE != pcode,,drop=FALSE] else X0[X0$PCODE == pcode,,drop=FALSE]
+    }
+    rn <- rownames(X0)
+    Y0 <- as.matrix(xtDur[[spp]][rn,])
+    ## make sure that columns (intervals) match up
+    stopifnot(all(colnames(Y0) == colnames(ltdur$x)))
+    stopifnot(length(setdiff(levels(X0$DURMETH), rownames(ltdur$end))) == 0)
+    ## interval end matrix
+    D <- ltdur$end[match(X0$DURMETH, rownames(ltdur$end)),]
+    ## exclude 0 sum and <1 interval rows
+    iob <- rowSums(Y0) > 0 & rowSums(!is.na(D)) > 1
+    if (sum(iob)==0)
+        return(structure("0 observation with multiple duration (1)",
+            class="try-error"))
+    if (sum(iob)==1)
+        return(structure("1 observation with multiple duration (2)",
+            class="try-error"))
+    X <- droplevels(X0[iob,])
+    Y0 <- Y0[iob,]
+    D <- D[iob,]
+    n <- nrow(D)
+    ## arranging counts into position
+    Yid <- ltdur$id[match(X$DURMETH, rownames(ltdur$id)),]
+    Y <- matrix(NA, n, ncol(ltdur$id))
+    for (i in seq_len(n)) {
+        w <- Yid[i,]
+        w <- w[!is.na(w)]
+        Y[i,seq_len(length(w))] <- Y0[i,w]
+    }
+    if (fit) {
+        res <- list()
+        for (i in seq_len(length(ff))) {
+            f <- as.formula(paste0("Y | D ", paste(as.character(ff[[i]]), collapse=" ")))
+            mod <- try(cmulti(f, X, type=type))
+            if (!inherits(mod, "try-error")) {
+                rval <- mod[c("coefficients","vcov","nobs","loglik")]
+                rval$p <- length(coef(mod))
+                rval$names <- NAMES[[i]]
+            } else {
+                rval <- mod
+            }
+            res[[names(NAMES)[i]]] <- rval
+        }
+        ## number of observations
+        #res$n <- n
+    } else {
+        res <- list(Y=Y, D=D, n=n)
+    }
+    res
+}
+
+Pcodes <- levels(pkDur$PCODE)
+
+#resDurBAMless1 <- list()
+resDurPcode1 <- list()
+for (i in 1:length(SPP)) {
+#    resDurBAMless1[[SPP[i]]] <- list()
+    resDurPcode1[[SPP[i]]] <- list()
+    for (j in 1:length(Pcodes)) {
+        cat("Singing rate (rem) estimation for", SPP[i], "in", Pcodes[j], "\n")
+        flush.console()
+#        resDurBAMless1[[SPP[i]]][[Pcodes[j]]] <- try(fitDurFun2(SPP[i], TRUE, type="rem",
+#            pcode=Pcodes[j], excl=TRUE))
+        resDurPcode1[[SPP[i]]][[Pcodes[j]]] <- try(fitDurFun2(SPP[i], TRUE, type="rem",
+            pcode=Pcodes[j], excl=FALSE))
+    }
+}
+save(resDurPcode1, #resDurBAMless1, 
+    file="~/Dropbox/bam/duration_ms/revisionOct2015/xval-Pcode1-rem.Rdata")
+
+#resDurBAMless1_mix <- list()
+resDurPcode1_mix <- list()
+for (i in 1:length(SPP)) {
+#    resDurBAMless1_mix[[SPP[i]]] <- list()
+    resDurPcode1_mix[[SPP[i]]] <- list()
+    for (j in 1:length(Pcodes)) {
+        cat("Singing rate (mix) estimation for", SPP[i], "in", Pcodes[j], "\n")
+        flush.console()
+#        resDurBAMless1_mix[[SPP[i]]][[Pcodes[j]]] <- try(fitDurFun2(SPP[i], TRUE, type="mix",
+#            pcode=Pcodes[j], excl=TRUE))
+        resDurPcode1_mix[[SPP[i]]][[Pcodes[j]]] <- try(fitDurFun2(SPP[i], TRUE, type="mix",
+            pcode=Pcodes[j], excl=FALSE))
+    }
+}
+save(resDurBAMless1_mix, resDurPcode1_mix,
+    file="~/Dropbox/bam/duration_ms/revisionOct2015/xval-Pcode1-mix.Rdata")
+
+
 ## 3-5-10 validation ----------------------------------------------
 
 
