778b505735663ba65bb17cb25a2168a4c39f5ae6
Peter Solymos <psolymos@gmail.com>
2016-03-03 13:42:58 -0700

    xv

--- R/qpad-duration-ms.R
+++ R/qpad-duration-ms.R
@@ -1,13 +1,15 @@
-## BAM-wise estimation -------------------------------------------
+## =============================================================================
+## load stuff from processing -----------------------------------------
 
 ### Preliminaries
 
 ## Define root folder where data are stored
 ROOT <- "c:/bam/May2015"
+ROOT2 <- "~/Dropbox/bam/duration_ms/revisionMarch2016"
 
 ## Load required packages
+library(MASS)
 library(mefa4)
-library(pbapply)
 library(detect)
 
 ## Load functions kept in separate file
@@ -15,6 +17,9 @@ source("~/repos/bamanalytics/R/dataprocessing_functions.R")
 
 ## Load preprocesses data
 load(file.path(ROOT, "out", "new_offset_data_package_2016-03-02.Rdata"))
+
+## =============================================================================
+## BAM-wise estimation -------------------------------------------
 
 ### Removal sampling
 
@@ -170,6 +175,190 @@ save(resDur, resDurData,
     file=file.path(ROOT, "out", "estimates_SRA_QPAD_v2016.Rdata"))
     #file=file.path(ROOT, "out", "estimates_SRA_QPAD_v2016_mix.Rdata"))
 
+## =============================================================================
+## summarize results a la QPAD (no distance sampling this time) --------------
+
+### Putting things together
+
+type <- "rem"
+
+## n.min is threshold above which all models are considered
+## n.con is threshold above which the 0 constant model is considered
+n.con <- 2
+n.min <- 25 # (max df = 5, 5*5=25)
+
+cat(type, "\n")
+if (type == "rem")
+    load(file.path(ROOT, "out", "estimates_SRA_QPAD_v2016.Rdata"))
+if (type == "mix")
+    load(file.path(ROOT, "out", "estimates_SRA_QPAD_v2016_mix.Rdata"))
+
+## 0/1 table for successful model fit
+sra_mod <- t(sapply(resDur, function(z)
+    ifelse(sapply(z, inherits, what="try-error"), 0L, 1L)))
+tmp <- rownames(sra_mod)
+sra_models <- matrix(0L, length(tmp), ncol(sra_mod))
+dimnames(sra_models) <- list(tmp, colnames(sra_mod))
+sra_models[rownames(sra_mod),] <- sra_mod
+## data deficient cases: dropped factor levels
+## need to check length of NAMES and length of COEF --> correct factor level
+## designation is possible, if != bump up AIC/BIC
+for (spp in tmp) {
+    for (mid in colnames(sra_models)) {
+        if (!inherits(resDur[[spp]][[mid]], "try-error")) {
+            if (type == "rem")
+                lcf <- length(resDur[[spp]][[mid]]$coefficients)
+            if (type == "mix")
+                lcf <- length(resDur[[spp]][[mid]]$coefficients) - 1
+            lnm <- length(resDur[[spp]][[mid]]$names)
+            if (lcf != lnm) {
+                cat("SRA conflict for", spp, "model", mid,
+                "( len.coef =", lcf, ", len.name =", lnm, ")\n")
+                sra_models[spp,mid] <- 0
+            }
+        } else {
+            resDur[[spp]][[mid]] <- structure("Error", class = "try-error")
+            #attributes(resDur[[spp]][[mid]]) <- NULL
+            #class(resDur[[spp]][[mid]]) <- "try-error"
+        }
+    }
+}
+## no constant model means exclude that species
+sra_models[sra_models[,1]==0,] <- 0L
+colSums(sra_models)
+
+sra_nmod <- ncol(sra_mod)
+
+## sample sizes
+sra_n <- numeric(length(tmp))
+names(sra_n) <- tmp
+sra_nn <- sapply(resDur, function(z) ifelse(inherits(z[["0"]], "try-error"),
+    NA, z[["0"]]$nobs))
+sra_n[names(sra_nn)] <- sra_nn
+
+## exclude all models for species with < n.con observations
+sra_models[sra_n < n.con, ] <- 0L
+## exclude non-constant model for species with n.con < observations < n.min
+sra_models[sra_n < n.min & sra_n >= n.con, 2:ncol(sra_models)] <- 0L
+table(rowSums(sra_models))
+
+## spp to keep
+spp <- tmp[rowSums(sra_models) > 0]
+
+if (FALSE) {
+spplist <- read.csv("c:/Users/Peter/Dropbox/bam/duration_ms/revisionMarch2016/duration-ms-species_2Mar2016_SM.csv")
+rownames(spplist) <- spplist$Species_ID
+spplist[spplist$Same_taxa != "",]
+spplist <- spplist[spplist$Same_taxa == "",]
+compare_sets(spp, rownames(spplist))
+
+e <- new.env()
+load(file.path(ROOT, "out", "new_offset_data_package_2016-03-02.Rdata"), envir=e)
+TAX <- e$TAX
+#TAX <- TAX[TAX$Species_ID %in% spp,]
+#rownames(TAX) <- TAX$Species_ID
+TAX <- nonDuplicated(TAX, Species_ID, TRUE)
+TAX <- TAX[spp,c("Species_ID", "English_Name", "Family_Sci")]
+rm(e)
+
+aa <- with(spplist, table(Family_Sci, Singing_birds == "TRUE"))
+aa[aa[,1]>0,]
+aa[aa[,2]>0,]
+TAX2 <- TAX[!(rownames(TAX) %in% rownames(spplist)), ]
+TAX2$Singing_birds <- TRUE
+TAX2$Singing_birds[!(TAX2$Family_Sci %in% rownames(aa[aa[,2]>0,]))] <- NA
+TAX2$Singing_birds[TAX2$Family_Sci %in% rownames(aa[aa[,1]>0,])] <- FALSE
+
+tax <- rbind(spplist[,colnames(TAX2)], TAX2)
+write.csv(tax, file="c:/Users/Peter/Dropbox/bam/duration_ms/revisionMarch2016/duration-ms-species_3Mar2016.csv")
+}
+tax <- read.csv("c:/Users/Peter/Dropbox/bam/duration_ms/revisionMarch2016/duration-ms-species_3Mar2016.csv")
+rownames(tax) <- tax$Species_ID
+compare_sets(spp, rownames(tax))
+spp <- sort(intersect(spp, rownames(tax[tax$Singing_birds,])))
+
+sra_models <- sra_models[spp,]
+sra_n <- sra_n[spp]
+
+## no. of parameters
+
+sra_df <- sapply(resDur[["OVEN"]][1:sra_nmod], "[[", "p")
+
+## estimates
+sra_estimates <- resDur[spp]
+
+## species table
+e <- new.env()
+load(file.path(ROOT, "out", "new_offset_data_package_2016-03-02.Rdata"), envir=e)
+tax2 <- e$TAX
+#rownames(tax2) <- tax2$Species_ID
+tax2 <- nonDuplicated(tax2, Species_ID, TRUE)
+spp_table <- data.frame(spp=spp,
+    scientific_name=tax2[spp, "Scientific_Name"],
+    common_name=tax2[spp, "English_Name"])
+rownames(spp_table) <- spp
+spp_table <- droplevels(spp_table)
+
+## get variable names for different models
+sra_list <- sapply(sra_estimates[["OVEN"]], function(z) paste(z$names, collapse=" + "))
+
+## loglik values
+sra_loglik <- sra_models
+sra_loglik[] <- -Inf
+for (i in spp) { # species
+    for (j in 1:sra_nmod) { # models
+        if (sra_models[i,j] > 0)
+            sra_loglik[i,j] <- resDur[[i]][[j]]$loglik
+    }
+}
+
+## AIC/BIC
+sra_aic <- sra_bic <- sra_loglik
+sra_aic[] <- Inf
+sra_bic[] <- Inf
+for (i in spp) {
+    sra_aic[i,] <- -2*sra_loglik[i,] + 2*sra_df
+    sra_bic[i,] <- -2*sra_loglik[i,] + log(sra_n[i])*sra_df
+}
+
+## model ranking
+sra_aicrank <- t(apply(sra_aic, 1, rank))*sra_models
+sra_aicrank[sra_aicrank==0] <- NA
+sra_bicrank <- t(apply(sra_bic, 1, rank))*sra_models
+sra_bicrank[sra_bicrank==0] <- NA
+
+sra_aicbest <- apply(sra_aicrank, 1, function(z) colnames(sra_models)[which.min(z)])
+sra_bicbest <- apply(sra_bicrank, 1, function(z) colnames(sra_models)[which.min(z)])
+
+version <- "3"
+
+bamcoefs <- list(spp=spp,
+    spp_table=spp_table,
+    sra_list=sra_list,
+    sra_models=sra_models,
+    sra_n=sra_n,
+    sra_df=sra_df,
+    sra_loglik=sra_loglik,
+    sra_aic=sra_aic,
+    sra_bic=sra_bic,
+    sra_aicrank=sra_aicrank,
+    sra_bicrank=sra_bicrank,
+    sra_aicbest=sra_aicbest,
+    sra_bicbest=sra_bicbest,
+    sra_estimates=sra_estimates,
+    version=version,
+    type=type)
+.BAMCOEFS <- list2env(bamcoefs)
+
+if (type == "rem") {
+    save(.BAMCOEFS, file=file.path(ROOT, "out", "BAMCOEFS_duration_rem.rda"))
+}
+if (type == "mix") {
+    save(.BAMCOEFS, file=file.path(ROOT, "out", "BAMCOEFS_duration_mix.rda"))
+}
+
+
+## =============================================================================
 ## project-wise estimation -------------------------------------------
 
 ## models to consider
@@ -250,7 +439,7 @@ for (i in 1:length(SPP)) {
     }
 }
 save(resDurPcode1, #resDurBAMless1, 
-    file="~/Dropbox/bam/duration_ms/revisionOct2015/xval-Pcode1-rem.Rdata")
+    file="~/Dropbox/bam/duration_ms/revisionMarch2016/xval-Pcode1-rem.Rdata")
 
 #resDurBAMless1_mix <- list()
 resDurPcode1_mix <- list()
@@ -266,11 +455,260 @@ for (i in 1:length(SPP)) {
             pcode=Pcodes[j], excl=FALSE))
     }
 }
-save(resDurBAMless1_mix, resDurPcode1_mix,
-    file="~/Dropbox/bam/duration_ms/revisionOct2015/xval-Pcode1-mix.Rdata")
+save(resDurPcode1_mix, #resDurBAMless1_mix, 
+    file="~/Dropbox/bam/duration_ms/revisionMarch2016/xval-Pcode1-mix.Rdata")
 
 
-## 3-5-10 validation ----------------------------------------------
+## =============================================================================
+## 3-5-10 validation variance / bias -----------------------------------------
+## bias/variance/MSE for m0/m0t and mb/mbt
+
+e <- new.env()
+load(file.path(ROOT2, "BAMCOEFS_duration_rem.rda"), envir=e)
+.BAMCOEFSrem <- e$.BAMCOEFS
+
+e <- new.env()
+load(file.path(ROOT2, "BAMCOEFS_duration_mix.rda"), envir=e)
+.BAMCOEFSmix <- e$.BAMCOEFS
+
+aic0 <- .BAMCOEFSrem$sra_aic
+aicb <- .BAMCOEFSmix$sra_aic
+SPP <- sort(intersect(rownames(aic0), rownames(aicb)))
+
+#cfall0 <- sapply(SPP, function(spp) exp(coefBAMspecies(spp, 0, 0)$sra))
+cfall0 <- sapply(SPP, function(spp) {
+    exp(unname(.BAMCOEFSrem$sra_estimates[[spp]][["0"]]$coefficients))
+    })
+dim(cfall0) <- c(length(SPP), 1)
+dimnames(cfall0) <- list(SPP, "phi_0")
+cfallb <- t(sapply(SPP, function(spp) {
+    tmp <- unname(.BAMCOEFSmix$sra_estimates[[spp]][["0"]]$coefficients)
+    c(phi_b=exp(tmp[1]), c=plogis(tmp[2]))
+    }))
+
+pkDur <- droplevels(pkDur[pkDur$DURMETH %in% c("G","R","Y","Ya","Z"),])
+rn <- intersect(rownames(pkDur), rownames(xtDur[[1]]))
+pkDur <- droplevels(pkDur[rn,])
+
+dput(colnames(xtDur[[1]]))
+
+ff <- list(
+    ~ 1,
+    ~ JDAY,
+    ~ TSSR,
+    ~ JDAY + I(JDAY^2),
+    ~ TSSR + I(TSSR^2),
+    ~ JDAY + TSSR,
+    ~ JDAY + I(JDAY^2) + TSSR,
+    ~ JDAY + TSSR + I(TSSR^2),
+    ~ JDAY + I(JDAY^2) + TSSR + I(TSSR^2),
+    ~ TSLS,
+    ~ TSLS + I(TSLS^2),
+    ~ TSLS + TSSR,
+    ~ TSLS + I(TSLS^2) + TSSR,
+    ~ TSLS + TSSR + I(TSSR^2),
+    ~ TSLS + I(TSLS^2) + TSSR + I(TSSR^2))
+names(ff) <- 0:14
+NAMES <- list(
+    "0"="INTERCEPT",
+    "1"=c("INTERCEPT", "JDAY"),
+    "2"=c("INTERCEPT", "TSSR"),
+    "3"=c("INTERCEPT", "JDAY", "JDAY2"),
+    "4"=c("INTERCEPT", "TSSR", "TSSR2"),
+    "5"=c("INTERCEPT", "JDAY", "TSSR"),
+    "6"=c("INTERCEPT", "JDAY", "JDAY2", "TSSR"),
+    "7"=c("INTERCEPT", "JDAY", "TSSR", "TSSR2"),
+    "8"=c("INTERCEPT", "JDAY", "JDAY2", "TSSR", "TSSR2"),
+    "9"=c("INTERCEPT", "TSLS"),
+    "10"=c("INTERCEPT", "TSLS", "TSLS2"),
+    "11"=c("INTERCEPT", "TSLS", "TSSR"),
+    "12"=c("INTERCEPT", "TSLS", "TSLS2", "TSSR"),
+    "13"=c("INTERCEPT", "TSLS", "TSSR", "TSSR2"),
+    "14"=c("INTERCEPT", "TSLS", "TSLS2", "TSSR", "TSSR2"))
+
+ymin <- 1
+B <- 1000
+
+spp <- "OVEN"
+
+xvfun <- function(spp, B=1000, ymin=1) {
+    Y <- groupSums(xtDur[[spp]][rn,], 2,
+        c("0-3", "xxx", "0-3", "0-3", "xxx", "xxx", "0-3", "0-3",
+        "xxx", "3-5", "3-5", "xxx", "xxx", "3-5", "5-10", "5-10",
+        "5-10", "5-10", "xxx", "5-10", "5-10", "5-10", "5-10", "5-10"))
+    Y <- as.matrix(Y)[,c("0-3","3-5","5-10")]
+    YY <- cbind("0-3"=Y[,1], "0-5"=Y[,1]+Y[,2], "0-10"=rowSums(Y))
+    YY <- YY[YY[,3] >= ymin,,drop=FALSE]
+    pkDurS <- pkDur[rownames(YY),]
+
+    cfi00 <- .BAMCOEFSrem$sra_estimates[[spp]][["0"]]$coefficients
+    vci00 <- .BAMCOEFSrem$sra_estimates[[spp]][["0"]]$vcov
+
+
+    cfi0b <- .BAMCOEFSmix$sra_estimates[[spp]][["0"]]$coefficients
+    vci0b <- .BAMCOEFSmix$sra_estimates[[spp]][["0"]]$vcov
+
+    best0 <- colnames(aic0)[which.min(aic0[spp,])]
+    bestb <- colnames(aicb)[which.min(aicb[spp,])]
+    X0 <- model.matrix(ff[[best0]], pkDurS)
+    Xb <- model.matrix(ff[[bestb]], pkDurS)
+
+    cf0 <- .BAMCOEFSrem$sra_estimates[[spp]][[best0]]$coefficients
+    vc0 <- .BAMCOEFSrem$sra_estimates[[spp]][[best0]]$vcov
+    cfb <- .BAMCOEFSmix$sra_estimates[[spp]][[bestb]]$coefficients
+    vcb <- .BAMCOEFSmix$sra_estimates[[spp]][[bestb]]$vcov
+
+    vci0b <- Matrix::nearPD(vci0b)$mat
+    vc0 <- Matrix::nearPD(vc0)$mat
+    vcb <- Matrix::nearPD(vcb)$mat
+
+    b_out <- list()
+    for (j in 1:B) {
+        phi00 <- exp(rnorm(1, cfi00, sqrt(vci00)))
+        pcf0b <- mvrnorm(1, cfi0b, vci0b)
+
+        pp_0 <- 1-exp(-c(3,5,10)*phi00)
+        pp_b <- 1-plogis(pcf0b[2])*exp(-c(3,5,10)*exp(pcf0b[1]))
+
+        YC3_0 <- (YY[,1] * pp_0[3]) / (pp_0[1] * YY[,3])
+        YC5_0 <- (YY[,2] * pp_0[3]) / (pp_0[2] * YY[,3])
+        YC3_b <- (YY[,1] * pp_b[3]) / (pp_b[1] * YY[,3])
+        YC5_b <- (YY[,2] * pp_b[3]) / (pp_b[2] * YY[,3])
+
+        ## time varying models
+
+        cft0 <- mvrnorm(1, cf0, vc0)
+        cftb <- mvrnorm(1, cfb, vcb)
+        phi0i <- exp(X0 %*% cft0)
+        phib <- exp(cftb[1])
+        cbi <- plogis(Xb %*% cftb[-1])
+        #phi0i <- exp(X0 %*% cf0)
+        #phib <- exp(cfb[1])
+        #scbi <- plogis(Xb %*% cfb[-1])
+
+        YC3i_0 <- (YY[,1] * (1-exp(-10*phi0i))) / ((1-exp(-3*phi0i)) * YY[,3])
+        YC5i_0 <- (YY[,2] * (1-exp(-10*phi0i))) / ((1-exp(-5*phi0i)) * YY[,3])
+        YC3i_b <- (YY[,1] * (1-cbi*exp(-10*phib))) / ((1-cbi*exp(-3*phib)) * YY[,3])
+        YC5i_b <- (YY[,2] * (1-cbi*exp(-10*phib))) / ((1-cbi*exp(-5*phib)) * YY[,3])
+
+        out <- list()
+        out$Y10 <- mean(YY[,3])
+        out$m0 <- c(YC3=mean(YC3_0), YC5=mean(YC5_0))
+        out$mb <- c(YC3=mean(YC3_b), YC5=mean(YC5_b))
+        out$m0t <- c(YC3=mean(YC3i_0), YC5=mean(YC5i_0))
+        out$mbt <- c(YC3=mean(YC3i_b), YC5=mean(YC5i_b))
+
+        b_out[[j]] <- unlist(out)
+    }
+
+    b_out <- do.call(rbind, b_out)
+
+    theta <- b_out[,1]
+    theta <- 1
+    theta_hat <- b_out[,-1]
+
+    MSE <- colSums((theta_hat - theta)^2) / B
+    Var <- colSums(t(t(theta_hat) - colMeans(theta_hat))^2) / B
+    Bias <- sqrt(MSE - Var)
+    data.frame(MSE=MSE, Var=Var, Bias=Bias)
+}
+xtfun <- function(spp, ymin=1) {
+    Y <- groupSums(xtDur[[spp]][rn,], 2,
+        c("0-3", "xxx", "0-3", "0-3", "xxx", "xxx", "0-3", "0-3",
+        "xxx", "3-5", "3-5", "xxx", "xxx", "3-5", "5-10", "5-10",
+        "5-10", "5-10", "xxx", "5-10", "5-10", "5-10", "5-10", "5-10"))
+    Y <- as.matrix(Y)[,c("0-3","3-5","5-10")]
+    YY <- cbind("0-3"=Y[,1], "0-5"=Y[,1]+Y[,2], "0-10"=rowSums(Y))
+    YY <- YY[YY[,3] >= ymin,,drop=FALSE]
+    nrow(YY)
+}
+
+nob <- pbsapply(SPP, xtfun, ymin=1)
+res <- list()
+for (spp in SPP) {
+cat(spp, date(), "\n");flush.console()
+res[[spp]] <- try(xvfun(spp))
+}
+
+save(res, nob, file=file.path(ROOT2, "var-bias-res.Rdata"))
+
+res2 <- res[!sapply(res, inherits, "try-error")]
+
+aaa <- data.frame(Var=as.numeric(sapply(res2, "[[", "Var")),
+    MSE=as.numeric(sapply(res2, "[[", "MSE")),
+    Bias=as.numeric(sapply(res2, "[[", "Bias")),
+    Model=rep(c("0","b","0t","bt"), each=2),
+    Duration=c("3","5"),
+    Species=as.factor(rep(names(res2), each=8)))
+aaa$n <- nob[match(aaa$Species, names(nob))]
+dim(aaa)
+aaa <- aaa[!is.na(aaa$Var) & aaa$n >= 2,]
+summary(aaa)
+dim(aaa)
+
+m1 <- lm(Var ~ Model + Duration + n, aaa[aaa$n >= 1,])
+m2 <- lm(Bias ~ Model + Duration + n, aaa[aaa$n >= 1,])
+a1 <- anova(update(m1, . ~ . + Species))
+a1$Perc <- 100 * a1[,"Sum Sq"] / sum(a1[,"Sum Sq"])
+a2 <- anova(update(m2, . ~ . + Species))
+a2$Perc <- 100 * a2[,"Sum Sq"] / sum(a2[,"Sum Sq"])
+summary(m1)
+a1
+summary(m2)
+a2
+
+ng <- 1:200
+sf <- function(x) quantile(x, 0.9, na.rm=TRUE)
+maxVar <- cbind(max3_0 = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "3" & aaa$Model == "0" & aaa$n >= z])),
+    max5_0 = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "5" & aaa$Model == "0" & aaa$n >= z])),
+    max3_b = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "3" & aaa$Model == "b" & aaa$n >= z])),
+    max5_b = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "5" & aaa$Model == "b" & aaa$n >= z])),
+    max3_t = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "3" & aaa$Model == "t" & aaa$n >= z])),
+    max5_t = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "5" & aaa$Model == "t" & aaa$n >= z])),
+    max3_bt = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "3" & aaa$Model == "bt" & aaa$n >= z])),
+    max5_bt = sapply(ng, function(z)
+        sf(aaa$Var[aaa$Duration == "5" & aaa$Model == "bt" & aaa$n >= z])))
+maxBias <- cbind(max3_0 = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "3" & aaa$Model == "0" & aaa$n >= z])),
+    max5_0 = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "5" & aaa$Model == "0" & aaa$n >= z])),
+    max3_b = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "3" & aaa$Model == "b" & aaa$n >= z])),
+    max5_b = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "5" & aaa$Model == "b" & aaa$n >= z])),
+    max3_t = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "3" & aaa$Model == "t" & aaa$n >= z])),
+    max5_t = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "5" & aaa$Model == "t" & aaa$n >= z])),
+    max3_bt = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "3" & aaa$Model == "bt" & aaa$n >= z])),
+    max5_bt = sapply(ng, function(z)
+        sf(aaa$Bias[aaa$Duration == "5" & aaa$Model == "bt" & aaa$n >= z])))
+
+par(mfrow=c(4,2))
+for (i in c(1,3,5,7)+1) {
+plot(ng, maxVar[,i], main=paste("Var", colnames(maxVar)[i]),
+    type="l", lwd=2, col=2, ylim=c(0, max(maxVar)))
+lines(ng, maxVar[,i-1], lty=2, col=2, lwd=2)
+plot(ng, maxBias[,i], main=paste("Bias", colnames(maxVar)[i]),
+    type="l", lwd=2, col=2, ylim=c(min(maxBias), max(maxBias)))
+lines(ng, maxBias[,i-1], lty=2, col=2, lwd=2)
+}
+
+## =============================================================================
+## species specific predictions plots
+
+
+
+
+
 
 
 
