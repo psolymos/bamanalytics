b1e64b76460d94ab836d77a246ed05c7c6138d81
Peter Solymos <psolymos@gmail.com>
2019-12-21 20:07:33 -0700

    coni

--- projects/coni-nwt/models.R
+++ projects/coni-nwt/models.R
@@ -35,23 +35,21 @@ proj4string(s) <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 #' # Extract GIS info
 #'
 #' This is the stack we will use for prediction
-i <- 6 # BCR 6
 ROOT <- "d:/bam/BAM_data_v2019/gnm"
-pr <- stack(file.path(ROOT, "data", "stacks", paste0("bcr", i, "_1km.grd")))
-#prc <- stack(file.path(ROOT, "data", "stacks", paste0("bcr", i, "cat_1km.grd")))
-prc <- stack(file.path(ROOT, "data", "_new", "cat2", paste0("bcr", i, "cat_1km.grd")))
-pr <- pr[[which(names(pr) != "bcr")]]
-prc <- prc[[which(names(prc) != "bcr")]]
-pr <- addLayer(pr, prc)
-rm(prc)
+pr60 <- stack(file.path(ROOT, "data", "subunits", paste0("bcr60all_1km.grd")))
+pr61 <- stack(file.path(ROOT, "data", "subunits", paste0("bcr61all_1km.grd")))
+pr <- mosaic(pr60, pr61, fun=mean)
+
 #' Reproject station object
 s <- spTransform(s, proj4string(pr))
-pt <- extract(pr, s)
+pt <- extract(pr, s, method="simple")
 rownames(pt) <- rownames(s@data)
+sum(is.na(pt))
+
 #' Check data for layers to drop: constant varoables (SD=0)
 SD <- apply(pt, 2, sd)
-ncol(pt) # 147
-sum(SD > 0) # down to 73
+ncol(pt) # 222
+sum(SD > 0) # down to 111
 #' Check data for layers to drop: highly correlated variables (r_max=0.9)
 get_cn <- function(z, rmax=0.9) {
     SD <- apply(z, 2, sd)
@@ -82,11 +80,11 @@ get_cn <- function(z, rmax=0.9) {
 }
 #' Keep NALC and lf, these are discrete
 CN <- unique(c("nalc", "lf", get_cn(pt[,SD > 0], rmax=0.9)))
-length(CN) # down to 46
+length(CN) # down to 50
 
 #' Organizing into a data frame, set NALC and lf as factors
 dat <- data.frame(
-    x[,c("detection", "Offset", "station", "datetime")],
+    x[,c("detection", "Offset", "station", "datetime","long","lat")],
     pt[match(x$station, rownames(pt)),CN])
 dat$nalc <- as.factor(dat$nalc) # !!! fixme -- strange values
 nlevels(dat$nalc)
@@ -106,4 +104,113 @@ rownames(vi) <- NULL
 vi$cumsum <- cumsum(vi$rel.inf)
 vi
 
+## analysis
 
+#    Randomly selected 10% of the recordings scanned
+#    Filtered out recordings that had booms detected in them
+#    Reviewed all recognizer detections for those recordings
+#    Assigned individual ID to each boom detection in those recordings
+# There's definitely more than one individual in some of these recordings.
+# Here's the distribution of the results:
+#    1 CONI: 29 sites
+#    2 CONI: 11 sites
+#    3 CONI: 2 sites
+#    4 CONI: 1 site
+# The one thing to keep in mind is that these are almost entirely
+# from the Alberta fire site. I suspect the densities will be lower at the
+# other province/treatment combos.
+
+library(mefa4)
+library(lme4)
+library(pbapply)
+
+load("~/GoogleWork/collaborations/coni-nwt/CONI-AB-NWT-datawithpredictors.RData")
+x <- read.csv("~/GoogleWork/collaborations/coni-nwt/CONI-AB-NWT-withOffsets.csv")
+x <- nonDuplicated(x, station, TRUE)
+dat$lat <- x$lat[match(dat$station, x$station)]
+dat$long <- x$long[match(dat$station, x$station)]
+rm(x)
+
+str(dat)
+
+tmp <- strsplit(as.character(dat$station), "-")
+n <- sapply(tmp, length)
+table(n)
+head(tmp[n==3])
+head(tmp[n==4])
+tmp[n==5]
+ss1 <- as.factor(sapply(tmp, function(z) paste0(z[1])))
+table(table(ss1))
+ss2 <- as.factor(sapply(tmp, function(z) paste0(z[1], "-", z[2])))
+table(table(ss2))
+ss3 <- as.factor(sapply(tmp, function(z) paste0(z[1], "-", z[2], "-", z[3])))
+table(table(ss3))
+
+plot(dat$long, dat$lat, col=ss3)
+
+yy <- rep(1:4, c(29, 11, 2, 1)) # mean 1.418605
+
+levs <- c("1"="ConTmp",
+    "2"="ConTai",
+    "5"="Dec",
+    "6"="Mix",
+    "8"="Shr",
+    "10"="Grs",
+    "14"="Wet",
+    "16"="Bar",
+    "18"="Wat")
+dat$alc <- dat$nalc
+levels(dat$alc) <- levs[levels(dat$nalc)]
+
+levs2 <- c("1"="ConTmp",
+    "2"="ConTai",
+    "5"="DM",
+    "6"="DM",
+    "8"="OP",
+    "10"="OP",
+    "14"="OP",
+    "16"="OP",
+    "18"="OP")
+dat$lc <- dat$nalc
+levels(dat$lc) <- levs2[levels(dat$lc)]
+
+addmargins(table(dat$alc, dat$lc)/30)
+
+m0 <- glmer(detection ~ (1 | station), data=dat,
+    offset=dat$Offset, family=binomial("cloglog"))
+m1 <- glmer(detection ~ lc + (1 | station), data=dat,
+    offset=dat$Offset, family=binomial("cloglog"))
+g0 <- glm(detection ~ 1, data=dat,
+    offset=dat$Offset, family=binomial("cloglog"))
+g1 <- glm(detection ~ lc, data=dat,
+    offset=dat$Offset, family=binomial("cloglog"))
+AIC(m0, m1, g0, g1)
+
+round(sort(exp(fixef(m1))), 3)
+round(sort(exp(coef(g1))), 3)
+
+#d <- mefa4::sum_by(dat$detection, dat$station)
+
+f <- function() {
+    d <- nonDuplicated(dat[sample.int(nrow(dat)),], station, TRUE)
+    m <- glm(detection ~ lc-1, data=d,
+        offset=d$Offset, family=binomial("cloglog"))
+    coef(m)
+}
+
+B <- 1000
+res <- t(pbreplicate(B, f()))
+
+cf <- fixef(m1)
+est <- exp(c(cf[1], cf[1]+cf[-1]))
+names(est) <- levels(dat$lc)
+est
+
+cf <- coef(g1)
+est2 <- exp(c(cf[1], cf[1]+cf[-1]))
+names(est2) <- levels(dat$lc)
+est2
+
+boxplot(exp(res))
+points(1:4,est, pch=4, col=2, cex=2)
+points(1:4,est2, pch=4, col=4, cex=2)
