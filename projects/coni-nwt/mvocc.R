8c9e42236f7ea2d8b74549e32e2ebceaf2e3a711
Peter Solymos <psolymos@gmail.com>
2020-01-14 16:19:01 -0700

    Update mvocc.R

--- projects/coni-nwt/mvocc.R
+++ projects/coni-nwt/mvocc.R
@@ -48,6 +48,7 @@ method="Nelder-Mead", inits, control=list(), hessian=FALSE, ...) {
     o
 }
 
+summary(t(replicate(100, {
 phi <- 0.5
 pd <- 0.7
 M <- 1000
@@ -63,6 +64,7 @@ Z <- matrix(1, M*J, 1)
 
 o <- mvocc(Y, X, Z)
 plogis(o$par)
+})))
 
 ## modified mv occupancy model
 ## Y: M x J matrix
@@ -115,23 +117,100 @@ method="Nelder-Mead", inits, control=list(), hessian=FALSE, ...) {
     o
 }
 
-M <- 1000
-J <- 10
+summary(t(replicate(100, {
+    M <- 1000
+    J <- 10
 
-p <- 0.8 # prob availability
-q <- 0.55 # prob use
-D <- 0.1 # density
-A <- 2^2*pi # effective area
+    p <- 0.8 # prob availability
+    q <- 0.55 # prob use
+    D <- 0.1 # density
+    A <- 2^2*pi # effective area
 
-W <- rbinom(M, 1, 1-exp(-D*A))
-table(W)
-Y <- matrix(0, M, J)
-for (j in 1:J)
-    Y[,j] <- rbinom(M, W, p*q)
+    W <- rbinom(M, 1, 1-exp(-D*A))
+    table(W)
+    Y <- matrix(0, M, J)
+    for (j in 1:J)
+        Y[,j] <- rbinom(M, W, p*q)
 
-X <- matrix(1, M, 1)
-Z <- matrix(1, M*J, 1)
+    X <- matrix(1, M, 1)
+    Z <- matrix(1, M*J, 1)
 
-o <- mvocc2(Y, X, Z, p, A)
-c(exp(o$par[1]), plogis(o$par[2]))
+    o <- mvocc2(Y, X, Z, p, A)
+    c(exp(o$par[1]), plogis(o$par[2]))
+})))
 
+
+## CONI specific model
+## p: availability from survival model
+## lambda: Poisson mean from conditional likelihood step
+mvocc3 <- function(Y, X, Z, p, lambda, weights,
+method="Nelder-Mead", inits, control=list(), hessian=FALSE, ...) {
+
+    good.num.limit <- c(.Machine$double.xmin, .Machine$double.xmax)^(1/3)
+
+    Y <- ifelse(Y > 0, 1, 0)
+
+    J <- ncol(Y)
+    M <- nrow(Y)
+    nDP <- ncol(Z)
+    nOP <- ncol(X)
+    nP <- nDP + nOP
+
+    if (missing(p))
+        p <- rep(1, M)
+    if (missing(lambda))
+        lambda <- rep(1, M)
+    if (missing(weights))
+        weights <- rep(1, M)
+    if (missing(inits))
+        inits <- rep(0, nP)
+
+    yvec <- as.numeric(Y)
+    navec <- is.na(yvec)
+    nd <- ifelse(rowSums(Y, na.rm = TRUE) == 0, 1, 0)
+    rowProds <- function (x, na.rm = FALSE)
+        exp(rowSums(log(x), na.rm = na.rm))
+
+    nll <- function(params) {
+        delta <- plogis(X %*% params[1:nOP])
+        psi <- delta * (1-exp(-lambda))
+        pvec <- plogis(Z %*% params[(nOP + 1):nP]) * p
+        cp <- (pvec^yvec) * ((1 - pvec)^(1 - yvec))
+        cp[navec] <- 1
+        cpmat <- matrix(cp, M, J)
+        loglik <- log(rowProds(cpmat) * psi + nd * (1 - psi))
+        loglik <- sum(loglik * weights)
+        if (!is.finite(loglik) || is.na(loglik))
+            loglik <- -good.num.limit[2]
+        -loglik
+    }
+
+    o <- optim(inits, nll,
+        method = method, hessian = hessian, control = control, ...)
+    o
+}
+
+summary(t(replicate(100, {
+    M <- 1000
+    J <- 10
+
+    p <- 0.8 # prob availability
+    q <- 0.55 # prob use
+    D <- 0.1 # density
+    A <- 2^2*pi # effective area
+    lambda <- A*D
+    delta <- 0.9 # ZI is 1-delta
+
+    N <- rpois(M, lambda) * rbinom(M, 1, delta)
+    W <- ifelse(N > 0, 1, 0)
+    table(W, N)
+    Y <- matrix(0, M, J)
+    for (j in 1:J)
+        Y[,j] <- rbinom(M, W, p*q)
+
+    X <- matrix(1, M, 1)
+    Z <- matrix(1, M*J, 1)
+
+    o <- mvocc3(Y, X, Z, p, lambda)
+    plogis(o$par)
+})))
